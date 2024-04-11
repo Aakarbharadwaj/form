0d0372abb6c0f52b33a7bcb7da6e5d3cba107098
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-04-11 15:17:34 +0530

    new commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Form from './components/Form'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">
         Form
       </h1>
-      <Form/>
+      <div>
+        <Form />
+      </div>
     </>
   )
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <input className="bg-slate-200 m-5 py-2" placeholder='First name' name='fn' />
+        </div>
+    )
+}
+
+export default Form
