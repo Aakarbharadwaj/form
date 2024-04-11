d724db77e44b0f66236684f0298e1e6577c2e6b2
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-04-11 15:38:23 +0530

    new commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const Form = () => {
+    const [formData, setFormData] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+    });
+
+    const changeHandler = (e) => {
+        setFormData((prevData) => {
+
+            return {
+                ...prevData,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
     return (
         <div>
-            <input className="bg-slate-200 m-5 py-2" placeholder='First name' name='fn' />
+            <input className="bg-slate-200 m-5 py-2" placeholder='First name' type='text' name='fname' onChange={changeHandler} />
+            <input className="bg-slate-200 m-5 py-2" placeholder='Last name' type='text' name='lname' onChange={changeHandler} />
+            <input className="bg-slate-200 m-5 py-2" placeholder='email' type='email' name='email' onChange={changeHandler} />
         </div>
     )
 }
