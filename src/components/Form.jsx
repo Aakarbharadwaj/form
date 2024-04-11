0bf6f3f7cf3ab0f7e7cb83c6a1fb59a858527380
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-04-11 16:14:53 +0530

    new commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,11 +22,14 @@ const Form = () => {
     return (
         <>
             <div className='border-solid border-slate-500 border-2 w-1/5 m-auto p-5'>
-                <input className="bg-slate-200 m-2 py-2" placeholder='First name' type='text' name='fname' onChange={changeHandler} /> <br />
-                <input className="bg-slate-200 m-2 py-2" placeholder='Last name' type='text' name='lname' onChange={changeHandler} /> <br />
-                <input className="bg-slate-200 m-2 py-2" placeholder='email' type='email' name='email' onChange={changeHandler} /> <br />
+                <input className="bg-slate-200 m-2 py-2" placeholder='First name' type='text' name='fname' value={formData.fname} onChange={changeHandler} /> <br />
+
+                <input className="bg-slate-200 m-2 py-2" placeholder='Last name' type='text' name='lname' value={formData.lname} onChange={changeHandler} /> <br />
+
+                <input className="bg-slate-200 m-2 py-2" placeholder='email' type='email' name='email' value={formData.email} onChange={changeHandler} /> <br />
+
                 <div>
-                    <button class="rounded-full px-6 py-1 text-sky-100 m-2 bg-sky-600  ">Submit</button>
+                    <input className="rounded-full px-6 py-1 text-sky-100 m-2 bg-sky-600 " type='submit' placeholder='submit' />
                 </div>
             </div>
         </>
