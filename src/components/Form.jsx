efd5cc2d89680bedd151f6fd897a4c7edfbefab4
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-04-11 15:55:32 +0530

    new commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,11 +20,16 @@ const Form = () => {
     }
 
     return (
-        <div>
-            <input className="bg-slate-200 m-5 py-2" placeholder='First name' type='text' name='fname' onChange={changeHandler} />
-            <input className="bg-slate-200 m-5 py-2" placeholder='Last name' type='text' name='lname' onChange={changeHandler} />
-            <input className="bg-slate-200 m-5 py-2" placeholder='email' type='email' name='email' onChange={changeHandler} />
-        </div>
+        <>
+            <div className='border-solid border-slate-500 border-2 w-1/5 m-auto'>
+                <input className="bg-slate-200 m-2 py-2" placeholder='First name' type='text' name='fname' onChange={changeHandler} /> <br />
+                <input className="bg-slate-200 m-2 py-2" placeholder='Last name' type='text' name='lname' onChange={changeHandler} /> <br />
+                <input className="bg-slate-200 m-2 py-2" placeholder='email' type='email' name='email' onChange={changeHandler} /> <br />
+            </div>
+            <div>
+                
+            </div>
+        </>
     )
 }
 
